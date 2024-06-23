3c09eeaf3be2730933cf14c63f496bdbaca0e86e
alpinn <alfinasyah36@gmail.com>
2024-06-23 23:13:33 +0700

    edit navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,49 +1,81 @@
-import React, { useState } from "react";
+import react, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false);
+  // const [nav, setNav] = useState(false);
 
-  const handleNav = () => {
-    setNav(!nav);
-  };
+  // const handleNav = () => {
+  //   setNav(!nav);
+  // };
+  const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
-    <div className="flex justify-between items-center h-24 max-w-full px-4">
-      <h1 className="w-full text-3xl font-bold text-[#3083ff]">Easy Car</h1>
-      <ul className="hidden md:flex">
-        <li className="">
-          <a href="#">Jadilah Perental</a>
-        </li>
-        <li className="">
-          <a href="#">Sewa Mobil</a>
-        </li>
-        <button className="p-2 bg-blue-300">Login</button>
-      </ul>
-      <div
-        onClick={handleNav}
-        className="block md:hidden"
-      >
-        {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
-      </div>
-      <div
-        className={
-          !nav
-            ? "fixed left-0 h-full top-0 w-[60%] border-r border-r-blue-600 bg-blue-500 ease-in-out duration-300"
-            : "fixed left-[-100%]"
-        }
-      >
-        <h1 className="w-full text-3xl font-bold text-white m-8">Easy Car</h1>
-        <ul className="uppercase p-4">
-          <li className="p-4 text-white border-b border-white">
-            Jadilah Perental
-          </li>
-          <li className="p-4 text-white border-b border-white">Sewa Mobil</li>
-          <button className="p-2 w-full my-5 bg-white  text-blue-700">
-            Login
-          </button>
-        </ul>
-      </div>
+    <div className="app">
+      <nav>
+        <div className="max-w-full mx-auto">
+          <div className="flex mx-auto justify-between w-5/6 ">
+            {/* Primary menu and logo */}
+            <div className="flex items-center gap-16 my-12">
+              {/* logo */}
+              <div>
+                <a
+                  href="/"
+                  className="flex gap-1 font-bold text-[#3083ff] items-center text-3xl"
+                >
+                  <span>Easy Car</span>
+                </a>
+              </div>
+              {/* primary */}
+              <div className="hidden md:flex gap-10 ">
+                <a
+                  href="#"
+                  className="mt-2"
+                >
+                  Jadilah Perental
+                </a>
+                <a
+                  href="#"
+                  className="mt-2"
+                >
+                  Sewa Mobil
+                </a>
+                <button className="rounded  text-white bg-blue-500 py-2 px-4 ">
+                  Login
+                </button>
+              </div>
+            </div>
+            {/* secondary */}
+            <div className="flex gap-6">
+              {/* Mobile navigation toggle */}
+              <div className="md:hidden flex items-center">
+                <button onClick={() => setToggleMenu(!toggleMenu)}>
+                  {!toggleMenu ? (
+                    <AiOutlineMenu size={20} />
+                  ) : (
+                    <AiOutlineClose size={20} />
+                  )}
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* mobile navigation */}
+        <div
+          className={`fixed z-40 w-full  bg-blue-500 overflow-hidden flex flex-col md:hidden gap-12  origin-top duration-700 ${
+            !toggleMenu ? "h-0" : "h-full"
+          }`}
+        >
+          <div className="px-14 py-8">
+            <div className="flex flex-col gap-8 font-bold tracking-wider text-white">
+              <a href="#">Jadilah Perental</a>
+              <a href="#">Sewa Mobil</a>
+              <button className="rounded  text-blue-700 bg-white py-2 px-4 ">
+                Login
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 };
